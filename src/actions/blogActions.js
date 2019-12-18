fa4db69a221492c8791aa211ba0a83176b38d58b
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-18 20:56:29 +0900

    アクション名の変更とpost処理の変更
    What
    アクション名の見直し
    Post処理の記述=> post後は/blogにpushする

--- src/actions/blogActions.js
+++ src/actions/blogActions.js
@@ -1,7 +1,7 @@
 import {push} from 'connected-react-router'
-export function fetchTodo(token){
+export function fetchArticle(token){
     return dispatch => {
-        dispatch({type: "FETCH_TODO_START"});
+        dispatch({type: "FETCH_ARTICLE_START"});
         fetch('http://localhost:8000/api/blog/', {
             method: 'GET',
             headers: {
@@ -12,49 +12,52 @@ export function fetchTodo(token){
         .then((res) => res.json())
         .then((data) => {
             if(data){
-                dispatch({type: "FETCH_TODO_SUCCESS", articles: data})
+                dispatch({type: "FETCH_ARTICLE_SUCCESS", articles: data})
             } else {
                 alert(data.message);
-                dispatch({type: "FETCH_TODO_ERROR", error: data.message})
+                dispatch({type: "FETCH_ARTICLE_ERROR", error: data.message})
             }
         })
         .catch((err) => {
-            dispatch({type: "FETCH_TODO_ERROR", error: err});
+            dispatch({type: "FETCH_ARTICLE_ERROR", error: err});
         })
     }
 }
 
-export function postTodo(title, body, user){
+export function ariticle_create(title, article, user){
     return dispatch => {
-        dispatch({type: "TODO_POST_START"});
+        dispatch({type: "ARTICLE_POST_START"});
+        //localstrage からtokenを取得する
         const token = localStorage.token;
+        //現在のユーザー情報idを取得
+        const user_id = user.id
 
-
-        fetch('http://localhost:8000/api/entries/', {
+        fetch('http://localhost:8000/api/blog/', {
     
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${token}`
+            },
             body: JSON.stringify(
                 {
                     title: title,
-                    body: body,
-                    user: user,
+                    article: article,
+                    user: user_id,
                 }
             )
         })
         .then((res) => res.json())
         .then((data) => {
             if(data){
-                var token = localStorage.token;
-                dispatch({type: "TODO_POST_SUCCESS", token: token});
-                dispatch(push('/todos'));
+                dispatch(push('/blog'));
             } else {
                 alert(data.message);
-                dispatch({type: "TODO_POST_ERROR", error: data.message})
+                dispatch({type: "ARTICLE_POST_ERROR", error: data.message})
             }
         })
         .catch((err) => {
-            dispatch({type: "TODO_POST_ERROR", errpr: err});
+            dispatch({type: "ARTICLE_POST_ERROR", errpr: err});
         })
     }
 }
