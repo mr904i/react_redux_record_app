00c24b5e65f95780499310010f83cace8dd43883
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-18 21:12:08 +0900

    Urlの変更とBlogNewコンポーネントの追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Route} from 'react-router-dom';
 import BlogList from './containers/BlogList';
+import BlogNew from './containers/BlogNew';
 import Login from './containers/Login';
 import {ConnectedRouter} from 'connected-react-router';
 
@@ -10,7 +11,8 @@ class App extends Component {
     return(
       <ConnectedRouter history={this.props.history}>
           <Route path="/" component={Login} exact={true} />
-          <Route path="/todos" component={BlogList} />
+          <Route path="/blog" component={BlogList} />
+          <Route path="/blog_new" component={BlogNew} />
       </ConnectedRouter>  
     );
   }
