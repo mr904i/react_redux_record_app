76522dac3ed1d32798cd0618b42fce272eeb2e08
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-20 15:01:18 +0900

    BlogUpdateコンポーネントの作成
    BlogNewコンポーネントをベースにBlogUpdateコンポーネントを作成した。
    Propsとして受け取った値はコンストラクターでthis.props.location.state.~で受け取り、各inputタグへのvalue
    に与えた。更新ボタンを押下後はarticle_updateアクションへdispatchさせ、更新に必要な値を引き渡した。

---
+++ src/containers/BlogUpdate.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+
+import { connect } from 'react-redux';
+
+import {article_update} from '../actions/blogActions'
+
+class BlogUpdate extends Component {
+    constructor(props){
+        debugger
+        super(props);
+        this.state = {
+            id: this.props.location.state.article_id,
+            title: this.props.location.state.article_title,
+            article: this.props.location.state.article_article
+        }
+    }
+    
+    handleTitleInput(e){
+        this.setState({title: e.target.value})
+    }
+
+    handleArticleInput(e){
+        this.setState({article: e.target.value})
+    }
+
+    clickUpdate(){
+        this.props.article_update(this.state.id, this.state.title, this.state.article, this.props.user);
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>記事投稿</h3>
+                
+                <div>
+                    <label>Title</label>
+                    <input 
+                        value = {this.state.title}
+                        onChange= {this.handleTitleInput.bind(this)}
+                        placeholder="Title"
+                    />
+                </div>
+                <div>
+                    <label>Article</label>
+                    <input
+                        value = {this.state.article}
+                        onChange= {this.handleArticleInput.bind(this)}
+                        placeholder="Article"
+                    />
+                </div>
+                <button onClick={this.clickUpdate.bind(this)}>Update Article</button>
+            </div>
+        )
+    }
+}
+
+//storeから取り出し
+const mapStateToProps = (state) => {
+    return{
+        user: state.auth.user
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        article_update: (id, title, article, user) => dispatch(article_update(id, title, article, user))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogUpdate)
