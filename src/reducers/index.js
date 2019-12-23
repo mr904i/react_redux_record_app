eda6ee7acb21ce458a0f61b289177b1ee610f16e
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-23 21:50:37 +0900

    reducerの作成
    非同期通信開始、エラー、成功アクションによってstoreに格納する値を変えた。
    非同期通信に成功した場合はactionで作成した連想配列をstoreに格納する。

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 import authReducer from './authReducers';
 import blogReducer from './blogReducers';
+import graphReducer from './graphReducers';
 
 const rootReducer = (history) => combineReducers({
     auth: authReducer,
     article: blogReducer,
+    graph: graphReducer,
     router: connectRouter(history)
 });
 
