3f4ed12ac4e35d0a0c197b19966ed4dd8e0b0277
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-25 20:10:36 +0900

    モーダル式カレンダーの導入
    生年月日を指定する際にカレンダーから入力できるように変更した。
    BirthDayCalendarMordalコンポーネントの中にBirthDayCalendarコンポーネントの中を配置した。SignUpコンポ
    ーネントからselectDateOfBirth関数をpropsとして渡してカレンダーの設定内容をpostできるようにした

---
+++ src/containers/BirthDayCalendarModal.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+import BirthDayCalendar from './BirthDayCalendar'
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+ }
+};
+
+Modal.setAppElement('#root') 
+class BirthDayCalendarModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+  afterOpenModal() {
+    this.subtitle.style.color = '#f00';
+  }
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.openModal}>SELECT YOUR BIRTHDAY</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 ref={subtitle => this.subtitle = subtitle}>SELECT YOUR BIRTH DAY</h2>
+          <BirthDayCalendar selectDateOfBirth={this.props.selectDateOfBirth} date_of_birth = {this.props.date_of_birth}/>
+          <div>Opend</div>
+            <button onClick={this.closeModal}>CLOSE</button>
+        </Modal>
+      </div>
+    );
+  }
+}
+export default BirthDayCalendarModal;
